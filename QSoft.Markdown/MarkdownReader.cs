281abfd8ad68492aa75917f0a0c49addff625688
Ben_Hsu <ben_hsu@ubiqconn.com>
2021-12-03 18:34:48 +0800

    加入專案檔案。

---
+++ QSoft.Markdown/MarkdownReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace QSoft.Markdown
+{
+    public class MarkdownReader
+    {
+        public List<MarkdownBasic> Open(Stream stream)
+        {
+            List<MarkdownBasic> datas = new List<MarkdownBasic>();
+            StreamReader sr = new StreamReader(stream);
+            string line = sr.ReadLine();
+            
+
+            return datas;
+        }
+
+    }
+
+    public class MarkdownBasic
+    {
+        public string Raw { set; get; }
+    }
+
+    public class Title
+    {
+        public int Size { set; get; }
+        public string Content { set; get; }
+    }
+}
